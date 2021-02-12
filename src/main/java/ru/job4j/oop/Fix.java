0fce996348062edd505e48e6181af755f9c23854
Kazakov Alexander <doublealex92@yandex.ru>
2021-02-13 01:36:06 +0500

    1.1.1. Конструктор по умолчанию [#411373]

---
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class Fix {
+    private String desk;
+
+    public Fix (String text) {
+        this.desk = text;
+    }
+
+    public static void main(String[] args) {
+        Fix jaja = new Fix ("jaja");
+
+    }
+}
